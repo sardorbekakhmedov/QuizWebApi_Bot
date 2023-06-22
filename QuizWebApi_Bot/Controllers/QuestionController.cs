3675f1f4ce6ad192e8f2cc127155b7fe609b7625
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-06-23 00:28:51 +0500

    add bot, user controllers

--- QuizWebApi_Bot/Controllers/QuestionController.cs
+++ QuizWebApi_Bot/Controllers/QuestionController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuizWebApi_Bot.Interfaces;
 using QuizWebApi_Bot.Models;
-using Telegram.Bot;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
 
 namespace QuizWebApi_Bot.Controllers;
 
@@ -12,98 +9,10 @@ namespace QuizWebApi_Bot.Controllers;
 public class QuestionController : ControllerBase
 {
     private readonly IQuestionRepository _questionRepository;
-    private readonly IUserRepository _userRepository;
 
-    public QuestionController(IQuestionRepository questionRepository, IUserRepository userRepository)
+    public QuestionController(IQuestionRepository questionRepository)
     {
         _questionRepository = questionRepository;
-        _userRepository = userRepository;
-    }
-
-    [HttpPost("bot")]
-    public async Task<IActionResult> PostUpdate([FromBody] Update update, CancellationToken cts)
-    {
-        var bot = new TelegramBotClient("5674695715:AAEGpPEyu_tUbeJp_C4slf89laNWGq9PQM0");
-
-        var (messageText, firstName, messageId, chatId, isSuccess) = GetMessage(update);
-
-        if (!isSuccess)
-            return BadRequest();
-
-        if (messageText == "/start")
-        {
-            await bot.SendTextMessageAsync(
-                chatId: chatId,
-                text: "🖐 Assalomu alekum, \nSizga har soatda bittadan savol yuboriladi",
-                cancellationToken: cts);
-
-            await _userRepository.AddUserAsync(chatId, firstName ?? "No name");
-        }
-        else if (messageText == "/result")
-        {
-            var result = await _userRepository.GetUserStatsAsync(chatId);
-            await bot.SendTextMessageAsync(
-                chatId: chatId,
-                text: result,
-                cancellationToken: cts);
-        }
-        else if (messageText is not null && messageText.StartsWith("!_?answer?_!"))
-        {
-
-            var user = await _userRepository.GetUserAsync(chatId);
-            string[] array = messageText.Split(',');
-
-            var questionId = Guid.Parse(array[1]);
-            var answerData = array[2];
-
-            await _userRepository.IncrementAnswerAsync(chatId);
-
-            //  await _userRepository.DeleteSentUserQuestionCollectionAsync(user, questionId);
-            var question = await _questionRepository.GetQuestionByIdAsync(questionId);
-
-            if (question != null)
-            {
-                if (question.CorrectAnswer == answerData)
-                {
-                    await bot.SendTextMessageAsync(
-                        chatId: chatId,
-                        text: "Qoyil 👍  javobingiz to'g'ri  ✅",
-                        cancellationToken: cts);
-
-                    await _userRepository.IncrementCorrectAnswerAsync(chatId);
-                }
-                else
-                {
-                    await bot.SendTextMessageAsync(
-                        chatId: chatId,
-                        text: $"Afsus 🥵  javob no to'g'ri  ❌  " +
-                               $"\n\n To'gri javob:  {question.CorrectAnswer}," +
-                               $"\n\nJavob ta'rifi:  {question.Description}",
-                        cancellationToken: cts);
-                }
-            }
-        }
-        else
-        {
-            await bot.SendTextMessageAsync(
-                chatId: chatId,
-                text: "$\"☢  No malum buyruq kiritildi!",
-                cancellationToken: cts);
-
-        }
-
-        return Ok();
-    }
-
-    private static (string? messageText, string? firstName, int messageId, long chatId, bool isSuccess) GetMessage(Update update)
-    {
-        if (update.Type == UpdateType.Message)
-            return (update.Message?.Text ?? "No text", update.Message?.From?.FirstName ?? "No Name", update.Message?.MessageId ?? -1, update.Message?.Chat.Id ?? -1, true);
-
-        if (update is { Type: UpdateType.CallbackQuery, CallbackQuery: { Data: not null, Message: not null } })
-            return (update.CallbackQuery.Data, update.CallbackQuery.From?.FirstName ?? "No Name", update.CallbackQuery.Message.MessageId, update.CallbackQuery.Message.Chat.Id, true);
-
-        return (null, null, -1, -1, false);
     }
 
     [HttpPost]
@@ -126,11 +35,6 @@ public class QuestionController : ControllerBase
         return Ok(await _questionRepository.UpdateQuestionAsync(questionId, model));
     }
 
-    [HttpGet("get_users")]
-    public async Task<IActionResult> GetAllUsers()
-    {
-        return Ok(await _userRepository.GetAllUsersAsync());
-    }
 
     [HttpGet("random_question")]
     public async Task<IActionResult> GetRandomQuestionAsync()
---
+++ QuizWebApi_Bot/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizWebApi_Bot.Interfaces;
+
+namespace QuizWebApi_Bot.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserController(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+
+
+    [HttpGet("get_users")]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        return Ok(await _userRepository.GetAllUsersAsync());
+    }
+}
